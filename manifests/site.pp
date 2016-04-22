9a30f622d129d14f64090f2c5fb348ff68477d4f
Adam Bottchen <adam@Adams-MacBook.corp.puppetlabs.net>
2016-04-22 10:01:11 -0700

    adding mod to agents

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,6 +25,10 @@ File { backup => false }
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
+node /agent/ {
+  include helloworld
+}
+
 node default {
   #incude a role on any node that specifies it's role via a trusted fact at provision time
   #https://docs.puppetlabs.com/puppet/latest/reference/lang_facts_and_builtin_vars.html#trusted-facts
