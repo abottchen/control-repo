3e10f2fa8fa6dbc8c2dd0bd1a752e7f6eddba313
Adam Bottchen <adam.bottchen@puppet.com>
2016-05-12 10:14:01 -0700

    removing helloworld

--- manifests/site.pp
+++ manifests/site.pp
@@ -37,5 +37,6 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': } 
-  include "helloworld"
+
+  notify {'Development Environment':}
 }
