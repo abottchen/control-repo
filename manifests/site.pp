c9f49f1e7f197cb594ad0618a2e48b224ef21785
Adam Bottchen <adam.bottchen@puppet.com>
2016-08-12 16:33:48 -0700

    removing filebucket test

--- manifests/site.pp
+++ manifests/site.pp
@@ -13,40 +13,19 @@
 
 # Disable filebucket by default for all File resources:
 #http://docs.puppetlabs.com/pe/latest/release_notes.html#filebucket-resource-no-longer-created-by-default
-#File { backup => false }
-if $trusted['certname'] == 'pe-201611-master-lei.puppetdebug.vlan' {
-  notify {'setting up master filebucket': }
-  filebucket { 'main': }
-} elsif $trusted['certname'] == 'pe-201611-cm-lei.puppetdebug.vlan' {
-  notify {'setting up compile master filebucket': }
-  filebucket { 'main': 
-    path   => false,
-    server => 'pe-201611-master-lei.puppetdebug.vlan',
-  }
-}
+File { backup => false }
+#if $trusted['certname'] == 'pe-201611-master-lei.puppetdebug.vlan' {
+#  notify {'setting up master filebucket': }
+#  filebucket { 'main': }
+#} elsif $trusted['certname'] == 'pe-201611-cm-lei.puppetdebug.vlan' {
+#  notify {'setting up compile master filebucket': }
+#  filebucket { 'main': 
+#    path   => false,
+#    server => 'pe-201611-master-lei.puppetdebug.vlan',
+#  }
+#}
 
-File { backup => 'main' }
-
-# DEFAULT NODE
-# Node definitions in this file are merged with node data from the console. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.
-
-# The default node definition matches any node lacking a more specific node
-# definition. If there are no other nodes in this file, classes declared here
-# will be included in every node's catalog, *in addition* to any classes
-# specified in the console for that node.
+#File { backup => 'main' }
 
 node default {
-  #incude a role on any node that specifies it's role via a trusted fact at provision time
-  #https://docs.puppetlabs.com/puppet/latest/reference/lang_facts_and_builtin_vars.html#trusted-facts
-  #https://docs.puppetlabs.com/puppet/latest/reference/ssl_attributes_extensions.html#aws-attributes-and-extensions-population-example
-
-  if !empty( $trusted['extensions']['pp_role'] ) {
-    include "role::${trusted['extensions']['pp_role']}"
-  }
-
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': } 
 }
