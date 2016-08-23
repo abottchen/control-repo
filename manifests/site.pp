5c43bc9c8c6091694cdb166cf0885672bf595a09
Adam Bottchen <adam.bottchen@puppet.com>
2016-08-22 17:22:26 -0700

    resetting production

--- manifests/site.pp
+++ manifests/site.pp
@@ -14,29 +14,7 @@
 # Disable filebucket by default for all File resources:
 #http://docs.puppetlabs.com/pe/latest/release_notes.html#filebucket-resource-no-longer-created-by-default
 #File { backup => false }
-if $trusted['certname'] == 'pe-201611-master-lei.puppetdebug.vlan' {
-  notify {'setting up master filebucket': }
-  filebucket { 'main': }
-} elsif $trusted['certname'] == 'pe-201611-cm-lei.puppetdebug.vlan' {
-  notify {'setting up compile master filebucket': }
-  filebucket { 'main': 
-    path   => false,
-    server => 'pe-201611-master-lei.puppetdebug.vlan',
-  }
-} elsif $trusted['certname'] == 'p6ip8m87picuyg6.delivery.puppetlabs.net' {
-  notify {'setting up agent filebucket': }
-  filebucket { 'main': 
-    path   => false,
-    server => 'pe-201611-master-lei.puppetdebug.vlan',
-  }
-}
-
-case $trusted['certname'] {
-  'pe-201611-master-lei.puppetdebug.vlan':    { File { backup => 'main' } }
-  'pe-201611-cm-lei.puppetdebug.vlan':        { File { backup => 'main' } }
-  'p6ip8m87picuyg6.delivery.puppetlabs.net':  { File { backup => 'main' } }
-  default:                                    { File { backup => false } }
-}
+File { backup => false }
 
 node default {
 }
