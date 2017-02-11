cbed5f16b4db47c98c1afd59d024a076be35428d
Adam Bottchen <adam.bottchen@puppet.com>
2017-02-10 20:37:13 -0800

    changes

--- site/zd24160/manifests/init.pp
+++ site/zd24160/manifests/init.pp
@@ -3,10 +3,7 @@ define print() {
 }
 
 class zd24160 {
-  #  $limits_edits = hiera_hash('linux_common::limits_edits')
+  $limits_edits = hiera_hash('linux_common::limits_edits')
 
-  $limits_edits = ["1","2","3"]
   print {$limits_edits:}
-
-  notify {'It ran':}
 }
