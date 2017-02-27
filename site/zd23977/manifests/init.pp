254a213b0909c9f579712428a224c24cf069d832
Adam Bottchen <adam.bottchen@puppet.com>
2017-02-27 15:21:42 -0800

    changes

--- site/zd23977/manifests/init.pp
+++ site/zd23977/manifests/init.pp
@@ -3,12 +3,18 @@ class zd23977 {
     root_password           => 'strongpassword',
     remove_default_accounts => true,
   }
-  
-  mysql_user { 'adam@127.0.0.1':
-    ensure                   => 'present',
-    max_connections_per_hour => '0',
-    max_queries_per_hour     => '0',
-    max_updates_per_hour     => '0',
-    max_user_connections     => '0',
+  #  
+  #  mysql_user { 'adam@127.0.0.1':
+  #    ensure                   => 'present',
+  #    max_connections_per_hour => '0',
+  #    max_queries_per_hour     => '0',
+  #    max_updates_per_hour     => '0',
+  #    max_user_connections     => '0',
+  #  }
+  mysql::db { 'mydb':
+    user        => 'myuser',
+    password   => 'mypass',
+    host    => 'localhost',
+    grant => ['SELECT', 'UPDATE'],
   }
 }
