3161ce947602de7717a2a64e6db1f12a1c79b240
Adam Bottchen <adam.bottchen@puppet.com>
2017-03-14 09:40:58 -0700

    changes

--- site/zd24458/manifests/init.pp
+++ site/zd24458/manifests/init.pp
@@ -1,5 +1,7 @@
 class zd24458 {
-  $clusters = {'a' => 1,'b' => 2,'c' => 3}
+  #  $clusters = {'a' => 1,'b' => 2,'c' => 3}
+  $query = ["=","certname", "pe-201642-master.puppetdebug.vlan"]
+  $clusters = puppetdb_query($query)
   file { '/tmp/test':
     ensure  => file,
     content => template('zd24458/test.txt.erb')
