0a8ab698bc88f0de3d6056831782acdea3772891
Adam Bottchen <adam.bottchen@puppet.com>
2017-03-14 09:48:08 -0700

    changes

--- site/zd24458/manifests/init.pp
+++ site/zd24458/manifests/init.pp
@@ -1,7 +1,6 @@
 class zd24458 {
-  #  $clusters = {'a' => 1,'b' => 2,'c' => 3}
   $query = ["from", "nodes"]
-  $clusters = puppetdb_query($query)[0]
+  $clusters = puppetdb_query($query)
   file { '/tmp/test':
     ensure  => file,
     content => template('zd24458/test.txt.erb')
