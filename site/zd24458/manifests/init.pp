7d0da5e70a4df06dac79d625dfb4d189f35dceee
Adam Bottchen <adam.bottchen@puppet.com>
2017-03-14 09:15:59 -0700

    changes

--- site/zd24458/manifests/init.pp
+++ site/zd24458/manifests/init.pp
@@ -1,5 +1,5 @@
 class zd24458 {
-  $clusters = ['a','b','c']
+  $clusters = ['a' => 1,'b' => 2,'c' => 3]
   file { '/tmp/test':
     ensure  => file,
     content => template('zd24458/test.txt.erb')
