0edb7877bb9111a0295229db665cf7debebff8b0
Adam Bottchen <adam.bottchen@puppet.com>
2017-03-22 16:03:06 -0700

    changes

--- site/hieratest/manifests/test.pp
+++ site/hieratest/manifests/test.pp
@@ -1,10 +1,10 @@
 class hieratest::test (
   $myarr = {},
 ) {
-  notify {"The ADB hash is: ${myarr}":}
+  #notify {"The ADB hash is: ${myarr}":}
   
-  $test = lookup("hieratest::test::myarr")
-  notify {"The lookup hash is: ${test}":}
+  #  $test = lookup("hieratest::test::myarr")
+  #notify {"The lookup hash is: ${test}":}
 
   $test3 = lookup("hieratest::test::myarr", Array[String], 'deep')
   notify {"The deep lookup hash is: ${test3}":}
