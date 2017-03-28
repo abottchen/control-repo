74dafe66bc0ac5852934efb59e08734f4f1a1426
Adam Bottchen <adam.bottchen@puppet.com>
2017-03-28 09:28:48 -0700

    changes

--- site/codemgmt_1055_hotfix/manifests/init.pp
+++ site/codemgmt_1055_hotfix/manifests/init.pp
@@ -1,5 +1,5 @@
 class codemgmt_1055_hotfix {
-  if ((versioncmp('2017.2.0', $::pe_server_version) > 0) && (versioncmp('2016.5.0', $::pe_server_version) < 0)){
+  if ((versioncmp('2017.2.0', $::pe_server_version) > 0) and (versioncmp('2016.5.0', $::pe_server_version) < 0)){
     file {'/opt/puppetlabs/server/apps/puppetserver/bin/generate-puppet-types.rb':
       ensure => file
       source => 'puppet:///modules/codemgmt_1055_hotfix/generate-puppet-types.rb'
