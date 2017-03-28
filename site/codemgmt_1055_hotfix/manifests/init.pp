8f2523ccd08b301928492cd654273b0cbc1a20c6
Adam Bottchen <adam.bottchen@puppet.com>
2017-03-28 09:30:38 -0700

    changes

--- site/codemgmt_1055_hotfix/manifests/init.pp
+++ site/codemgmt_1055_hotfix/manifests/init.pp
@@ -1,8 +1,8 @@
 class codemgmt_1055_hotfix {
   if ((versioncmp('2017.2.0', $::pe_server_version) > 0) and (versioncmp('2016.5.0', $::pe_server_version) < 0)){
     file {'/opt/puppetlabs/server/apps/puppetserver/bin/generate-puppet-types.rb':
-      ensure => file
-      source => 'puppet:///modules/codemgmt_1055_hotfix/generate-puppet-types.rb'
+      ensure => file,
+      source => 'puppet:///modules/codemgmt_1055_hotfix/generate-puppet-types.rb',
       mode   => '0755',
       owner  => 'root',
       group  => 'root',
