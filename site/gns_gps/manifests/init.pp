67ad136849472483e47c0a6761aefb14247135c9
Adam Bottchen <adam.bottchen@puppet.com>
2017-03-28 13:56:14 -0700

    changes

--- site/gns_gps/manifests/init.pp
+++ site/gns_gps/manifests/init.pp
@@ -1,3 +1,18 @@
 class gns_gps {
-  notify {'gns_gps': }
+  file {'/tmp/test': 
+    ensure  => file,
+    content => "Don't restart",
+    notify  => Service['ntpd'],
+  }
+
+  service {'ntpd':
+    ensure   => running,
+    schedule => 'maint window',
+  }
+
+  schedule {'maint window':
+    range => '1-3',
+    period => 'daily',
+    repeat => 1,
+  }
 }
