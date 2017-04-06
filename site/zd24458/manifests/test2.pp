ac93fda7be25154a942ab09b847dfd1509fbefed
Adam Bottchen <adam.bottchen@puppet.com>
2017-04-06 16:09:23 -0700

    changes

--- site/zd24458/manifests/test2.pp
+++ site/zd24458/manifests/test2.pp
@@ -1,12 +1,12 @@
 class zd24458::test2 {
   $nodes =  {
-    "foo.example.com": {
-      "ipaddress": "192.168.0.2",
-      "osfamily": "Redhat"
+    "foo.example.com" => {
+      "ipaddress"     => "192.168.0.2",
+      "osfamily"      => "Redhat"
     },
-    "bar.example.com": {
-      "ipaddress": "192.168.0.3",
-      "osfamily": "Debian"
+    "bar.example.com" => {
+      "ipaddress"     => "192.168.0.3",
+      "osfamily"      => "Debian"
     }
   }
 
