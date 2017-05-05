875af663ad20c460331189c7df76f1b0fa179562
Adam Bottchen <adam.bottchen@puppet.com>
2017-05-05 09:46:31 -0700

    changes

--- site/typetest/lib/puppet/provider/tmpfile/bash.rb
+++ site/typetest/lib/puppet/provider/tmpfile/bash.rb
@@ -1,14 +1,17 @@
 Puppet::Type.type(:tmpfile).provide(:bash) do
   def create()
+    Puppet.debug("README: touch /tmp/#{@resource[:name]}")
     `touch /tmp/#{@resource[:name]}`
   end
 
   def destroy()
+    Puppet.debug("README: rm /tmp/#{@resource[:name]}")
     `rm /tmp/#{@resource[:name]}`
   end
 
   def exists?()
+    Puppet.debug("README: ls /tmp/#{@resource[:name]}")
     `ls /tmp/#{@resource[:name]} 2> /dev/null`
-    return $?.exitstatus == 0 ? true : false
+    return $?.exitstatus == 0 ? true && Puppet.debug("README: Exists") : false && Puppet.debug("README: Doesn't exist")
   end
 end
