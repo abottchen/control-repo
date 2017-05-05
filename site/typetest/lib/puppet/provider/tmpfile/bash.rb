eb9792d381863f9e7bb81bfa054662ca7711b2e1
Adam Bottchen <adam.bottchen@puppet.com>
2017-05-05 11:30:21 -0700

    changes

--- site/typetest/lib/puppet/provider/tmpfile/bash.rb
+++ site/typetest/lib/puppet/provider/tmpfile/bash.rb
@@ -1,7 +1,9 @@
 Puppet::Type.type(:tmpfile).provide(:bash) do
   def create()
-    Puppet.debug("README: echo #{@resource[:insides]} > /tmp/#{@resource[:name]}")
-    `echo #{@resource[:insides]} > /tmp/#{@resource[:name]}`
+    Puppet.debug("README: creating /tmp/#{@resource[:name]} with contents '#{@resource[:insides]}'")
+    File.open("/tmp/#{@resource[:name]}") do |f|
+      f.write(@resource[:insides]
+    end
     @property_hash[:ensure] = :present
   end
 
